3bb6672011e8a731ce96351703af0b05621a50c3
OceanDiaali <ocean.diaali@gmail.com>
2019-05-28 17:57:10 +0100

    first commit

---
+++ MonuMarksNigeria/DetailsPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace MonuMarksNigeria
+{
+    public partial class DetailsPage : ContentPage
+    {
+
+        public DetailsPage(string selectedState)
+        {
+            InitializeComponent();
+
+           var wikiUrl = "https://en.wikipedia.org/wiki/" + selectedState + "_state";
+           var videoUrl = "https://www.youtube.com/results?search_query=" + selectedState + "+state";
+
+        
+            SelectedWiki.Text = "Read about " + selectedState;
+            SelectedVideo.Text = "Watch videos of " + selectedState;
+            SelectedMap.Text = "Placeholder map for " + selectedState;
+
+            SelectedWiki.GestureRecognizers.Add(new TapGestureRecognizer() 
+            {
+                Command = new Command(() => 
+                {
+                    Device.OpenUri(new Uri(wikiUrl)); 
+                })
+            });
+
+            SelectedVideo.GestureRecognizers.Add(new TapGestureRecognizer()
+            {
+                Command = new Command(() =>
+                {
+                    Device.OpenUri(new Uri(videoUrl));
+                })
+            });
+
+            SelectedMap.GestureRecognizers.Add(new TapGestureRecognizer()
+            {
+                Command = new Command(() =>
+                {
+                    Map.OpenAsync(9.0882, 7.4934, new MapLaunchOptions
+                    {
+                        Name = "Test",
+                        NavigationMode = NavigationMode.Driving
+                    });
+                })
+            });
+
+
+        } // constructor
+
+
+    }
+}
---
+++ MonuMarksNigeria/MainPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+using Xamarin.Essentials;
+
+namespace MonuMarksNigeria
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+            MoveImage();
+        }
+
+        void MoveImage() 
+        {
+           //if (Arrow.Y == 100) 
+           //{
+                //Arrow.TranslateTo(0, -100, 3000);
+            //} else 
+            //{
+               Arrow.TranslateTo(0, 100, 3000);
+            //}
+        }
+
+
+
+        async void Handle_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //throw new NotImplementedException();
+            var picker = (Picker)sender;
+            int selectedIndex = picker.SelectedIndex;
+
+           
+
+
+            if (selectedIndex != -1) 
+            { 
+                var selection = (string)picker.ItemsSource[selectedIndex];
+                var detailPage = new DetailsPage(selection);
+                //detailPage.BindingContext = selection;
+                //BindingContext = selection;
+                await Navigation.PushAsync(detailPage);
+                //await Navigation.PushAsync(new DetailsPage());
+            }
+
+
+
+            ;
+        
+
+        //if (selectedIndex != -1) 
+        //{
+        //    await Browser.OpenAsync("https://en.wikipedia.org/wiki/"+selectedIndex.ToString()+"_state"); 
+        //}
+
+
+        //if (selectedIndex != -1)
+        //{
+        //    SelectedState.Text = "Taking you to " + (string)picker.ItemsSource[selectedIndex] + " page..";
+        //    SelectedState.IsVisible = true;
+        //}
+    } // handle event method
+
+    } // main class
+} // namespace
